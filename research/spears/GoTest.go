6051f40ac9e439dd64b0a7b7463d28a1eb285851
Aaron Spears <aaronspears@72.192.21.198.tigernet.wifi.dyn.clemson.edu>
2023-02-06 08:03:38 -0500

    Added research for Aaron Spears

---
+++ research/spears/GoTest.go
@@ -0,0 +1,13 @@
+// testing go functions and learning how to run the code
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	var x string = "Hello World!"
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+	}
+	fmt.Println(x)
+}
