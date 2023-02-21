a54c4bd9a04922240d7dc135ae3065e0ad6a81e2
Ryan Redler <rredler@clemson.edu>
2023-02-21 01:02:41 -0500

    Fix unresolved merge in App.tsx

--- application/frontend/src/pages/App.tsx
+++ application/frontend/src/pages/App.tsx
@@ -3,23 +3,15 @@ import '../styles/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CreateAccount from '../pages/Login/CreateAccountComponent';
 import Header from '../components/Header/Header';
-<<<<<<< HEAD
 import Faq from '../pages/FAQ/Faq';
-=======
 import Footer from '../components/FooterComponent'
->>>>>>> main
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
       <Header colorTheme={"light"} loggedIn={true} loginUser={"Van Scoy"}/>
-      <Faq/>
-=======
-          <Header colorTheme={"light"} loggedIn={true} loginUser={"Van Scoy"}/>
-            <CreateAccount/>
-          <Footer colorTheme={"light"}/>
->>>>>>> main
+        <CreateAccount/>
+      <Footer colorTheme={"light"}/>
     </div>
 
   )
