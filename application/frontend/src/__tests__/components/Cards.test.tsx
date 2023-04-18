13f3d55486c0c78d9334245d29607ab269e0c434
Aaron Spears <ajspear@clemson.edu>
2023-04-18 15:07:36 +0000

    Merged PR 68: New and improved dashboard tests
    Added tests for each user type to ensure that conditional rendering is working properly
    
    Related work items: #815, #816

--- application/frontend/src/__tests__/components/Cards.test.tsx
+++ application/frontend/src/__tests__/components/Cards.test.tsx
@@ -9,9 +9,47 @@ import PointsHistoryCard from '../../components/DashboardCards/PointsHistoryCard
 import ProfileCard from '../../components/DashboardCards/ProfileCard';
 import ReportsCard from '../../components/DashboardCards/ReportsCard';
 import { UserClaims } from '../../utils/getUserClaims';
+import LogsCard from '../../components/DashboardCards/LogsCard';
 
-test('ApplicationsCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
+//Driver Tests
+test('CatalogCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
+    render(<CatalogCard {...claims} />);
+  
+    const linkElement = screen.getByText("Catalog");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('PointsHistoryCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
+    render(<PointsHistoryCard {...claims} />);
+  
+    const linkElement = screen.getByText("Points History");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('NotificationsCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
+    render(<NotificationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Notifications");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('OrdersCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
+    render(<OrdersCard {...claims} />);
+  
+    const linkElement = screen.getByText("Orders");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('ApplicationsCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
 
     render(<ApplicationsCard {...claims} />);
   
@@ -20,53 +58,8 @@ test('ApplicationsCard logged in with user "test"', () => {
     expect(linkElement).toBeInTheDocument();
 });
 
-test('CatalogCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
-    render(<CatalogCard {...claims} />);
-  
-    const linkElement = screen.getByText("Catalog");
-    
-    expect(linkElement).toBeInTheDocument();
-});
-
-test('NotificationsCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
-    render(<NotificationsCard {...claims} />);
-  
-    const linkElement = screen.getByText("Notifications");
-    
-    expect(linkElement).toBeInTheDocument();
-});
-
-test('OrdersCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
-    render(<OrdersCard {...claims} />);
-  
-    const linkElement = screen.getByText("Orders");
-    
-    expect(linkElement).toBeInTheDocument();
-});
-
-test('OrganizationsCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
-    render(<OrganizationsCard {...claims} />);
-  
-    const linkElement = screen.getByText("Organizations");
-    
-    expect(linkElement).toBeInTheDocument();
-});
-
-test('PointsHistoryCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
-    render(<PointsHistoryCard {...claims} />);
-  
-    const linkElement = screen.getByText("Points History");
-    
-    expect(linkElement).toBeInTheDocument();
-});
-
-test('ProfileCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
+test('ProfileCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
     render(<ProfileCard {...claims} />);
   
     const linkElement = screen.getByText("Profile");
@@ -74,11 +67,160 @@ test('ProfileCard logged in with user "test"', () => {
     expect(linkElement).toBeInTheDocument();
 });
 
-test('ReportsCard logged in with user "test"', () => {
-    var claims: UserClaims = { authorized: true, role: 0, user: "test", id: 1, }
+test('OrganizationsCard logged in with user "testDriver"', () => {
+    var claims: UserClaims = { authorized: true, role: 0, user: "testDriver", id: 1, }
+    render(<OrganizationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Organizations");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+//Sponsor Tests
+
+test('CatalogCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+    render(<CatalogCard {...claims} />);
+  
+    const linkElement = screen.getByText("Catalog");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('PointsHistoryCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+    render(<PointsHistoryCard {...claims} />);
+  
+    const linkElement = screen.getByText("Points History");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('NotificationsCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+    render(<NotificationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Notifications");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('OrdersCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+    render(<OrdersCard {...claims} />);
+  
+    const linkElement = screen.getByText("Orders");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('ApplicationsCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+
+    render(<ApplicationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Applications");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('ProfileCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+    render(<ProfileCard {...claims} />);
+  
+    const linkElement = screen.getByText("Profile");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('OrganizationsCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
+    render(<OrganizationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Organizations");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('ReportsCard logged in with user "testSponsor"', () => {
+    var claims: UserClaims = { authorized: true, role: 1, user: "testSponsor", id: 2, }
     render(<ReportsCard {...claims} />);
   
     const linkElement = screen.getByText("Reports");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+//Admin Tests
+
+test('CatalogCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<CatalogCard {...claims} />);
+  
+    const linkElement = screen.getByText("Catalog");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('PointsHistoryCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<PointsHistoryCard {...claims} />);
+  
+    const linkElement = screen.getByText("Points History");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('NotificationsCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<NotificationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Notifications");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('OrdersCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<OrdersCard {...claims} />);
+  
+    const linkElement = screen.getByText("Orders");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('ProfileCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<ProfileCard {...claims} />);
+  
+    const linkElement = screen.getByText("Profile");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('OrganizationsCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<OrganizationsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Organizations");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('ReportsCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<ReportsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Reports");
+    
+    expect(linkElement).toBeInTheDocument();
+});
+
+test('LogsCard logged in with user "testAdmin"', () => {
+    var claims: UserClaims = { authorized: true, role: 2, user: "testAdmin", id: 3, }
+    render(<LogsCard {...claims} />);
+  
+    const linkElement = screen.getByText("Logs");
     
     expect(linkElement).toBeInTheDocument();
 });
